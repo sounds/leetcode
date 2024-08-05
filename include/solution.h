152a244e6c3419bfee1a42f1d4af05bb7cd22d1b
sounds <niuben_1234@163.com>
2024-08-05 17:18:13 +0800

    update

--- include/solution.h
+++ include/solution.h
@@ -1259,6 +1259,9 @@ public:
         }
 
     }
+
+
+
     /*
     int search(vector<int>& nums, int target) {
 
@@ -1285,6 +1288,47 @@ public:
 
     }
     */
+    /*
+    35. Search Insert Position
+    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+    You must write an algorithm with O(log n) runtime complexity.
+    Example 1:
+
+    Input: nums = [1,3,5,6], target = 5
+    Output: 2
+    Example 2:
+
+    Input: nums = [1,3,5,6], target = 2
+    Output: 1
+    Example 3:
+
+    Input: nums = [1,3,5,6], target = 7
+    Output: 4
+    
+
+    Constraints:
+
+    1 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    nums contains distinct values sorted in ascending order.
+    -104 <= target <= 104
+    */
+    bool _35 = true;
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                if(mid == 0 || nums[mid - 1] < target)
+                    return mid;
+                right = mid - 1;
+            }
+        }
+        return nums.size();
+    }
 
     /**
     41. First Missing Positive
