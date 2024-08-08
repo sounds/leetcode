1825329e74ec7a0c0c511fb82b75fe8ca35037c6
sounds <niuben_1234@163.com>
2024-08-08 17:24:37 +0800

    update

--- include/solution.h
+++ include/solution.h
@@ -4085,9 +4085,43 @@ public:
     Input: 1234567891
     Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
     **/
+    bool _273 = true;
     string numberToWords(int num)
     {
         return "";
+    }
+    /*
+    274. H-Index
+    Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+    According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+    Example 1:
+
+    Input: citations = [3,0,6,1,5]
+    Output: 3
+    Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+    Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+    Example 2:
+
+    Input: citations = [1,3,1]
+    Output: 1
+    
+
+    Constraints:
+
+    n == citations.length
+    1 <= n <= 5000
+    0 <= citations[i] <= 1000
+    */
+    bool _274 = true;
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        for(int i = 0; i < citations.size(); ++i) {
+            if(i + 1 > citations[i])
+                return i;
+        }
+        return citations.size();
     }
 
     /**
